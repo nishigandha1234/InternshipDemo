068ec380fc5db30616b3f1157f487aa5de1c0c11
Nishigandha kakade <140388910+nishigandha1234@users.noreply.github.com>
2025-08-23 15:15:24 +0530

    commit-3

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Result from addition method: " + addition(12, 4));
+        System.out.println("Result from division method: " + division(12, 4));
+    }
+
+    public static int addition(int a, int b) {
+        int ans = a + b;
+        return ans;  
+    }
+
+    public static int division(int a, int b) {
+        int div = a / b;
+        return div;  
+    }
+}
